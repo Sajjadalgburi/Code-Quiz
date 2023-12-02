535470f3350cf0b71b1cb5c9da63da1b0bb702a5
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-02 15:32:52 -0700

    Selected all elements i was going to use

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var topBar = document.getElementById("topBar");
+var mainContent = document.getElementById("mainContent");
+var startQuiz = document.getElementById("startQuiz");
+var timer = document.getElementById("timer");
