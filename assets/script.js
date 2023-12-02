c2275814b222e4eb420d01da51ff5171c9f6af93
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-02 15:53:58 -0700

    Fixing the countDown function

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,22 @@
-var topBar = document.getElementById("topBar");
-var mainContent = document.getElementById("mainContent");
-var startQuiz = document.getElementById("startQuiz");
-var timer = document.getElementById("timer");
+const topBar = document.getElementById("topBar");
+const mainContent = document.getElementById("mainContent");
+const startQuiz = document.getElementById("startQuiz");
+const timerText = document.getElementById("timer");
+let countdown; // Declare countdown variable globally
+
+startQuiz.addEventListener("click", function () {
+  countDown(75); // Start the timer with a duration of 75 seconds
+});
+
+function countDown(duration) {
+  let timer = duration;
+  countdown = setInterval(function () {
+    timerText.textContent = timer;
+    timer--;
+
+    if (timer < 0) {
+      clearInterval(countdown);
+      timerText.textContent = "Time's up!";
+    }
+  }, 1000);
+}
