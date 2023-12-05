b3a62444f25e1d2c02cda2850aebc1d97994ee65
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-04 20:04:59 -0700

    Implemented quiz Qs with validation

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,21 @@ var incorrectAnswer; // Declare incorrectAnswer variable globally to store incor
 let currentQuestionIndex = 0; // Declare currentQuestionIndex variable to store the current question index
 let score = 0; // Declare score variable to store the score of the quiz
 
+let questionNumber = 0;
+
+const quizQuestions = [
+  {
+    question: "This is Q one",
+    choices: ["1", "2", "3"],
+    answer: "2",
+  },
+  {
+    question: "This is Q 2 one",
+    choices: ["4", "5", "6"],
+    answer: "6",
+  },
+];
+
 // Event listener for the "Start Quiz" button
 startQuiz.addEventListener("click", function () {
   countDown(75); // Start the timer with a duration of 75 seconds
@@ -40,23 +55,39 @@ function countDown(duration) {
 }
 
 function displayPhase2() {
+  seccondPhase.innerHTML = "";
   mainContent.setAttribute("style", "display: none;"); // Hide the main content
   seccondPhase.setAttribute("style", "display: flex;"); // Display the second phase
 
   let h2El = document.createElement("h2"); // <h2></h2>
-  h2El.innerHTML = "Question 1"; // <h2>Question 1</h2>
+  // h2El.innerHTML = "Question 1"; // <h2>Question 1</h2>
+  h2El.innerHTML = quizQuestions[questionNumber].question;
 
   let div = document.createElement("div"); // <div> </div>
   div.setAttribute("id", "answerButtons"); // <div id = "answerButtons"> </div>
   div.setAttribute("class", "answerButtons"); // <div id="answerButtons" class="answerButtons"> </div>
 
-  for (let i = 0; i < 4; i++) {
+  let choices = quizQuestions[questionNumber].choices;
+  console.log(choices);
+
+  for (let i = 0; i < choices.length; i++) {
     let button = document.createElement("button"); // <button></button>
-    button.innerHTML = "answer " + i; // <button>answer 1</button>
+    button.innerHTML = choices[i]; // <button>answer 1</button>
     button.addEventListener("click", function (event) {
       event.preventDefault();
       console.log(event.target.innerHTML);
+      let selectedAns = event.target.innerHTML;
       // if statment
+      let correctAnswer = quizQuestions[questionNumber].answer;
+      if (selectedAns === correctAnswer) {
+        alert("correct answer");
+        questionNumber += 1;
+        displayPhase2();
+      } else {
+        alert("inccorect");
+        questionNumber += 1;
+        displayPhase2();
+      }
     });
     div.append(button); // <div id="answerButtons" class="answerButtons"><button>answer 1</button> </div>;
   }
