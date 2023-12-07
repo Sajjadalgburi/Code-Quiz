131c78b130140d2028b8d2d04699fac7a55848d9
Sajjadalgburi <dfgsajjad@gmail.com>
2023-12-07 15:43:19 -0700

    TESTING

--- assets/highscore.js
+++ assets/highscore.js
@@ -1,30 +1,40 @@
-// function highScore() {
-//   var myAnchor = document.getElementById("myAnchor");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const container = document.getElementById("appendHighscore");
 
-//   var retrievedInitials = localStorage.getItem("initials");
-//   var retrievedScore = localStorage.getItem("finalScore");
+//   // Retrieve existing high scores from local storage or initialize an empty array
+//   const existingHighScores =
+//     JSON.parse(localStorage.getItem("highScores")) || [];
 
-//   myAnchor.addEventListener("click", function (event) {
-//     // Prevent the default behavior of the anchor (e.g., navigating to a new page)
-//     event.preventDefault();
+//   // Retrieve the user's latest score from local storage
+//   const userScore = {
+//     initials: localStorage.getItem("initials") || "",
+//     score: parseInt(localStorage.getItem("finalScore")) || 0,
+//   };
 
-//     // Your custom code here
-//     alert("Anchor clicked!");
-//   });
+//   // Check if the user has played (valid initials and score greater than 0)
+//   if (userScore.initials && userScore.score > 0) {
+//     // Check if the user's score is not already in the existing high scores
+//     const userAlreadyInHighScores = existingHighScores.some(
+//       (score) =>
+//         score.initials === userScore.initials && score.score === userScore.score
+//     );
 
-//   var appendHighscore = document.getElementById("appendHighscore");
-//   var orderedListEl = document.createElement("ol");
-
-//   for (let i = 0; i < 6; i++) {
-//     var listItemEl = document.createElement("li");
-//     orderedListEl.appendChild(listItemEl);
+//     if (!userAlreadyInHighScores) {
+//       // Add the user's latest score to the existing high scores
+//       existingHighScores.push(userScore);
+//     }
 //   }
 
-//   appendHighscore.appendChild(orderedListEl);
-// }
+//   // Sort high scores in descending order based on the score
+//   existingHighScores.sort((a, b) => b.score - a.score);
 
-// highScore();
+//   // Save the updated high scores back to local storage
+//   localStorage.setItem("highScores", JSON.stringify(existingHighScores));
 
-// var test = document.getElementById("appendHighscore");
-
-// console.log(test);
+//   // Loop through the sorted high scores and append them to the container
+//   for (let i = 0; i < existingHighScores.length; i++) {
+//     let scoreItem = document.createElement("p");
+//     scoreItem.textContent = `${existingHighScores[i].initials} - ${existingHighScores[i].score}`;
+//     container.appendChild(scoreItem);
+//   }
+// });
